56c11bd0234b7e6de22ac11f405b3b9fb644e84f
ligmajohn <87954688+ligmajohn@users.noreply.github.com>
2023-03-07 15:13:24 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -40,7 +40,7 @@ clamp, round, heartbeat, renderstepped, stepped = math.clamp, math.round, Servic
 instnew, cfnew, v3new, sort, pack, unpack = Instance.new, CFrame.new, Vector3.new, table.sort, table.pack, table.unpack
 import = function(url)
 	local success, result = pcall(function() return game:HttpGet(format("https://raw.githubusercontent.com/ligmajohn/compat/main/libraries/%s.lua", gsub(lower(tostring(url)), " ", "-"))) end)
-	if success then return loadstring(game:HttpGet(result))() else return loadstring(game:HttpGet(url))() end
+	if success then return loadstring(result)() else return loadstring(game:HttpGet(url))() end
 end
 Maid = import("maid")
 Signal = import("signal")
