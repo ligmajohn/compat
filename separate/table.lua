7f085f4583e33b4dd966acf63b2694e43c9cd9e4
ligmajohn <87954688+ligmajohn@users.noreply.github.com>
2023-04-06 17:22:33 -0400

    Create table.lua

---
+++ separate/table.lua
@@ -0,0 +1,20 @@
+local filter = function(tbl, func)
+    local new = {}
+    for i, v in next, tbl do if func(i, v) then new[#new + 1] = v end end
+    return new
+end
+
+local map = function(tbl, func)
+    local new = {}
+    for i, v in next, tbl do
+        local k, x = func(i, v)
+        new[x or #new + 1] = k
+    end
+    return new
+end
+
+local merge = function(...)
+    local new = {}
+    for i, v in next, {...} do for _, v2 in next, v do new[i] = v2 end end
+    return new
+end
