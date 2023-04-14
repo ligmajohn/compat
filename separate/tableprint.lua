5926650f3a2b4ca10ba66147cc98c8ea37f80219
ligmajohn <87954688+ligmajohn@users.noreply.github.com>
2023-04-14 19:07:22 -0400

    Update tableprint.lua

--- separate/tableprint.lua
+++ separate/tableprint.lua
@@ -1,26 +1,21 @@
-local tab = -1
-
-function it(t)
-    local s = ""
-    tab = tab + 1
-    for i,v in next,t do 
-        local a = typeof(v)
-        s = s.."\n"..tostring(tab ~= 0 and string.rep("    ", tab) or "", (((a == "table" or a == "function" or not tonumber(i)) and tab ~= 0) and tostring(i).." = " or (tab == 0 and a == "table") and "start-" or "")..(a=="Instance" and "game."..v:GetFullName() or tostring(v)))
-        print(tab ~= 0 and string.rep("    ", tab) or "", (((a == "table" or a == "function" or not tonumber(i)) and tab ~= 0) and tostring(i).." = " or (tab == 0 and a == "table") and "start-" or "")..(a=="Instance" and "game."..v:GetFullName() or tostring(v)))  
-        if a == "table" then 
-            it(v)
+local t2s = function(...)
+    local res = ""
+    local tab = -1
+    local it
+    it = function(t)
+        local s = ""
+        tab = tab + 1
+        for i, v in next,t do 
+            local a = typeof(v)
+            s = s.."\n"..tostring(tab ~= 0 and string.rep("    ", tab) or "", (((a == "table" or a == "function" or not tonumber(i)) and tab ~= 0) and tostring(i).." = " or (tab == 0 and a == "table") and "start-" or "")..(a=="Instance" and "game."..v:GetFullName() or tostring(v)))
+            res = res .. (tab ~= 0 and string.rep("    ", tab) or "") .. " " .. ((((a == "table" or a == "function" or not tonumber(i)) and tab ~= 0) and tostring(i).." = " or (tab == 0 and a == "table") and "start-" or "")..(a=="Instance" and "game."..v:GetFullName() or tostring(v))) .. "\n"
+            if a == "table" then  it(v) end
         end
+        tab = tab - 1
+        return s
     end
-    tab = tab - 1
-    return s
+    it({...})
+    return res
 end
 
-function newprint(...)
-    local args = {...}
-    local ret = it(args)
-    return ret
-end
-
-newprint(getgenv())
-
-return newprint
+print(t2s(getgenv()))
